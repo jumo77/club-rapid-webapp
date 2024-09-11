198ac81826526c23eedc2f3cd1149787f2265918
jumo77 <jumo.kang77@gmail.com>
2024-09-11 10:51:38 +0900

    path corrected

--- src/App.js
+++ src/App.js
@@ -24,12 +24,12 @@ export default function App() {
                 <Navbar/>
                 <Switch>
                     <Route path="/" exact render={() => <Main/>}/>
-                    <Route path={backend} exact
+                    <Route path={"/"+backend} exact
                            render={() => <Page title={"Back-End"} brief={"서버를 만드는 기술."} url={backend}/>}/>
-                    <Route path={frontend} exact
+                    <Route path={"/"+frontend} exact
                            render={() => <Page title={"Front-End"} brief={"웹페이지를 만드는 기술."} url={frontend}/>}/>
-                    <Route path={mobile} exact render={() => <Page brief={"점유율 1위, 성장률 1위 플렛폼."} url={mobile}/>}/>
-                    <Route path={profile} exact render={() => <Profile/>}/>
+                    <Route path={"/"+mobile} exact render={() => <Page brief={"점유율 1위, 성장률 1위 플렛폼."} url={mobile}/>}/>
+                    <Route path={"/"+profile} exact render={() => <Profile/>}/>
                     <Route path="/" component={Error}/>
                 </Switch>
             </div>
